e7a4dd93a7f89fb8e614b30da31fae51c196d2fa
Mikiyas <educationforlife424@gmail.com>
2024-02-26 01:28:59 +0300

    Environs first

--- add_chain_built-ins.c
+++ add_chain_built-ins.c
@@ -87,72 +87,7 @@ int _my_current_working_cd_cmd(information_t *info)
 
 
 
-/**
- * unset_alias - sets alias to string
- * @info: parameter struct
- * @str: the string alias
- *
- * Return: Always 0 on success, 1 on error
- */
-int unset_alias(information_t *info, char *str)
-{
-	char *p, c;
-	int ret;
 
-	p = my_strchr(str, '=');
-	if (!p)
-		return (1);
-	c = *p;
-	*p = 0;
-	ret = delete_node_at_index(&(info->alias),
-		get_node_index(info->alias, node_starts_with(info->alias, str, -1)));
-	*p = c;
-	return (ret);
-}
-
-/**
- * set_alias - sets alias to string
- * @info: parameter struct
- * @str: the string alias
- *
- * Return: Always 0 on success, 1 on error
- */
-int set_alias(information_t *info, char *str)
-{
-	char *p;
-
-	p = my_strchr(str, '=');
-	if (!p)
-		return (1);
-	if (!*++p)
-		return (unset_alias(info, str));
-
-	unset_alias(info, str);
-	return (add_node_end(&(info->alias), str, 0) == NULL);
-}
-
-/**
- * print_alias - prints an alias string
- * @node: the alias node
- *
- * Return: Always 0 on success, 1 on error
- */
-int print_alias(list_t *node)
-{
-	char *p = NULL, *a = NULL;
-
-	if (node)
-	{
-		p = my_strchr(node->str, '=');
-		for (a = node->str; a <= p; a++)
-			my_putchar(*a);
-		my_putchar('\'');
-		my_puts(p + 1);
-		my_puts("'\n");
-		return (0);
-	}
-	return (1);
-}
 
 /**
  * _myhelp - changes the current directory of the process
